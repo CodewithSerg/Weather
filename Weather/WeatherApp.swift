43374ac8b401e5afdc788e3fe02beab22a003579
Sershm62 <68350510+Sershm62@users.noreply.github.com>
2020-12-27 21:08:22 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Sergey Antoniuk on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
